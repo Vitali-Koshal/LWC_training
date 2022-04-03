e59f4f720e495f41d799a9cc4d9a99f61d61f0eb
Vitali-Koshal <koshel.vit.rost@gmail.com>
2022-04-03 18:32:34 +0300

    add update task 2 and task3

--- force-app/main/default/lwc/childB/childB.js
+++ force-app/main/default/lwc/childB/childB.js
@@ -1,6 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
 export default class ChildB extends LightningElement {
-    @api getCheckBoxValue=false;
-    value=false;
+    @api getCheckBoxValue = false;
+    value = false;
 }
