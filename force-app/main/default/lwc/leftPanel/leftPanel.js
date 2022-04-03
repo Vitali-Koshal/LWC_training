d95bce7330e3b781ba944c0f2ff0c48a62de7007
Vitali-Koshal <koshel.vit.rost@gmail.com>
2022-04-03 18:33:38 +0300

    add update

--- force-app/main/default/lwc/leftPanel/leftPanel.js
+++ force-app/main/default/lwc/leftPanel/leftPanel.js
@@ -1,3 +1,29 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { publish, MessageContext} from 'lightning/messageService';
+import CHECKBOX_UPDATE_CHANNEL from '@salesforce/messageChannel/Checkbox_Updated__c';
 
-export default class LeftPanel extends LightningElement {}
+
+export default class LeftPanel extends LightningElement {
+    @wire(MessageContext)
+    messageContext;
+    checkbox1Value = false;
+    checkbox2Value = false;
+
+    handleCheckbox1State(event) {
+        this.checkbox1Value = event.target.checked;
+        const payload = {
+            state1: this.checkbox1Value,
+            state2: this.checkbox2Value
+        };
+        publish(this.messageContext, CHECKBOX_UPDATE_CHANNEL, payload);
+    }
+
+    handleCheckbox2State(event) {
+        this.checkbox2Value = event.target.checked;
+        const payload = {
+            state1: this.checkbox1Value,
+            state2: this.checkbox2Value
+        };
+        publish(this.messageContext, CHECKBOX_UPDATE_CHANNEL, payload);
+    }
+}
--- force-app/main/default/lwc/rightPanel/rightPanel.js
+++ force-app/main/default/lwc/rightPanel/rightPanel.js
@@ -1,3 +1,38 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire} from 'lwc';
+import { subscribe, MessageContext} from 'lightning/messageService';
+import CHECKBOX_UPDATE_CHANNEL from '@salesforce/messageChannel/Checkbox_Updated__c';
+export default class RightPanel extends LightningElement {
+    state1;
+    state2;
+    checkbox1VisibilityValue = 'slds-visible';
+    checkbox2VisibilityValue = 'slds-visible';
+    @wire(MessageContext)
+    messageContext;
 
-export default class RightPanel extends LightningElement {}
+    subscribeToMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext, 
+            CHECKBOX_UPDATE_CHANNEL, 
+            (message) => this.handleMessage(message)
+        );
+    }
+    handleMessage(message) {
+        this.state1 = message.state1;
+        this.state2 = message.state2;
+        if (this.state1 == false) {
+            this.checkbox1VisibilityValue = 'slds-visible';
+        }
+        else {
+            this.checkbox1VisibilityValue = 'slds-hidden';
+        }
+        if (this.state2 == false) {
+            this.checkbox2VisibilityValue = 'slds-visible';
+        }
+        else {
+            this.checkbox2VisibilityValue = 'slds-hidden';
+        }
+    }
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+}
